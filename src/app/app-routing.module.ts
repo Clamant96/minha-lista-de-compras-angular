53846f5f16dae83ed4036e6ff22991a32b8aaddc
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-05-02 15:03:15 -0300

    Estilizacao de componentes e contrucao de reteamento da aplicacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionComponent } from './section/section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: SectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
